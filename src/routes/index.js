7d44c6d107893ae9c06725f490baaf883d188d20
billie89-33 <bowvorn@gmail.com>
2026-05-28 15:42:43 +0700

    feat(order): implement order module with product snapshotting, atomic stock reduction, and mock payment status

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { router as productsRoutes } from "./product.route.js";
 import { router as cartRoutes } from "./cart.route.js";
+import { router as orderRoutes } from "./order.route.js";
 
 export const router = Router();
 
 router.use("/products", productsRoutes);
 router.use("/cart", cartRoutes);
+router.use("/orders", orderRoutes);
